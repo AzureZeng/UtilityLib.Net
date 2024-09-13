f101045d5ad0a37d6848d71e32673d41947d38b8
Azure Zeng <weedycn@outlook.com>
2024-09-13 15:06:41 +0800

    Add DataTableRenderer

---
+++ src/UtilityLib/JsonDataTableRenderer.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Azure Zeng. All rights reserved.
+// Licensed under the Apache License 2.0. See LICENSE in the project root for license information.
+
+using System.Data;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace AzureZeng.UtilityLib;
+
+public class JsonDataTableRenderer : JsonConverter<DataTable> {
+    public override DataTable? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) {
+        throw new JsonException("Only the serialization operation of DataTable is supported.");
+    }
+
+    public override void Write(Utf8JsonWriter writer, DataTable value, JsonSerializerOptions options) {
+        writer.WriteStartArray();
+        foreach (DataRow r in value.Rows) {
+            writer.WriteStartObject();
+            for (int i = 0; i < value.Columns.Count; i++) {
+                var c = value.Columns[i];
+                writer.WritePropertyName(c.ColumnName);
+                var val = r[i];
+                if (val is null or DBNull) {
+                    writer.WriteNullValue();
+                } else
+                    JsonSerializer.Serialize(writer, val, val.GetType(), options);
+            }
+            writer.WriteEndObject();
+        }
+        writer.WriteEndArray();
+    }
+}
---
+++ src/UtilityLib/UtilityLibJsonConverterResolver.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Azure Zeng. All rights reserved.
+// Licensed under the Apache License 2.0. See LICENSE in the project root for license information.
+
+using System.Data;
+using System.Text.Json;
+using System.Text.Json.Serialization.Metadata;
+
+namespace AzureZeng.UtilityLib;
+
+public class UtilityLibJsonConverterResolver : IJsonTypeInfoResolver {
+    public JsonTypeInfo? GetTypeInfo(Type type, JsonSerializerOptions options) {
+        if (type == typeof(DataTable)) {
+            var conv = options.Converters.FirstOrDefault(x => x.Type == typeof(DataTable));
+            return JsonMetadataServices.CreateValueInfo<DataTable>(options, conv ?? new JsonDataTableRenderer());
+        }
+        return null;
+    }
+}
